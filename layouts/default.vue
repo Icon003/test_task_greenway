40a3c3d85dee6f30e265a12f0a77703871ccb667
Бурбас Иван <Icon003@yandex.by>
2021-10-24 21:02:50 +0300

    Дефолтный layout

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="app">
+		<div class="app__wrapper">
+			<Nuxt />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+	* {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+	.app {
+		width: 100%;
+		height: 100vh;
+	}
+	.app__wrapper {
+		width: 100%;
+		max-width: 1440px;
+		height: 100%;
+		margin: 0 auto;
+	}
+
+</style>
